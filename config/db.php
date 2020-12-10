a7a62073e312d863c3b1e3882fec07132c540765
OUREAlexandra <OUREAlexandra@users.noreply.github.com>
2020-12-10 14:23:29 +0100

    fix deployment

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+
+define('APP_DB_USER', 'vanilla_sto_8308');
+define('APP_DB_PWD', 'oWnOnyhcm5kYv6XMqJgD');
+define('APP_DB_HOST', 'aa8a6f29-bdd8-47af-8dbd-b13d899f9a56.vanilla-sto-8308.mysql.dbs.scalingo.com:32066/vanilla_sto_8308?useSSL=true&verifyServerCertificate=false');
+define('APP_DB_NAME', 'vanilla_sto_8308');
